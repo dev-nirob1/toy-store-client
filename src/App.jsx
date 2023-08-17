93ab2f7f37f3a6c0ccd6b8580ed7658e8a60380b
dev-nirob1 <dev-nirob1@gmail.com>
2023-08-17 22:04:47 +0600

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h2 className='text-center text-red-500 font-bold'>hello world</h2>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,29 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import Home from "../Pages/Home/Home/Home";
+import Register from "../Pages/Register/Register";
+import Login from "../Pages/Login/Login";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App />,
+    //   errorElement: <
+    children: [
+        {
+            path: "/",
+            element: <Home />
+        },
+        {
+            path: "/login",
+            element: <Login />
+        },
+        {
+            path: "register",
+            element: <Register />
+        }
+    ]
+    },
+  ]);
+
+export default router;
