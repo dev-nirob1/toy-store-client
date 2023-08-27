9a0bd32dbc59b5ad9c534673c78fc7f1b35d0b54
dev-nirob1 <dev-nirob1@gmail.com>
2023-08-27 15:44:29 +0600

    login and ragistration page design complete

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 const Login = () => {
     const handleLogin = event => {
@@ -8,37 +9,59 @@ const Login = () => {
         console.log(email, password)
     }
     return (
-        <div className="w-full md:w-1/2 bg-white bg-opacity-90 p-8 rounded-md shadow-md">
-            <h1 className="text-2xl font-semibold mb-4">Login</h1>
-            <form className="space-y-4">
-                <input
-                    type="email"
-                    className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
-                    placeholder="Email"
-                />
-                <input
-                    type="password"
-                    className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
-                    placeholder="Password"
-                />
-                <button
-                    type="submit"
-                    className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring"
-                >
-                    Login
-                </button>
-            </form>
-            <div className="mt-4 text-center">
-                <a href="#" className="text-blue-500 underline">
-                    Don't have an account? Register here.
-                </a>
-            </div>
-            <div className="mt-4 text-center">
-                <button
-                    className="bg-red-600 text-white py-2 px-4 rounded-md hover:bg-red-700 focus:outline-none focus:ring"
-                >
-                    Sign in with Google
-                </button>
+        <div className="flex h-screen bg-gradient-to-r from-gray-500 to-gray-800">
+            <div className="m-auto md:w-1/3">
+                <div className="bg-white px-8 py-6 rounded shadow-md">
+                    <h2 className="text-center text-2xl font-bold mb-6">Login</h2>
+                    <form onSubmit={handleLogin}>
+                        <div className="mb-4">
+                            <label
+                                className="block text-gray-700 text-sm font-bold mb-2"
+                                htmlFor="email"
+                            >
+                                Email
+                            </label>
+                            <input
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                name="email"
+                                id="email"
+                                type="email"
+                                placeholder="Enter your email"
+                            />
+                        </div>
+                        <div className="mb-6">
+                            <label
+                                className="block text-gray-700 text-sm font-bold mb-2"
+                                htmlFor="password"
+                            >
+                                Password
+                            </label>
+                            <input
+                                className="shadow appearance-none border round5ed w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                name="password"
+                                id="password"
+                                type="password"
+                                placeholder="Enter your password"
+                            />
+                        </div>
+                        <input type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded focus:outline-none btn-block focus:shadow-outline" value="Login" />
+                    <button
+                        className="bg-red-500 hover:bg-red-700 text-white font-semibold py-2 px-4 rounded focus:outline-none btn-block focus:shadow-outline mt-5"
+                        type="button"
+                    >
+                        Sign in with Google
+                    </button>
+                    </form>
+                    <div className="text-center mt-4">
+                    <span>Don`t have an account? <Link className="text-blue-500" to="/register">Register</Link></span>
+                        {/* <Link
+                            to="/register"
+                            className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+                        >
+                            Create an account
+                        </Link> */}
+                    </div>
+                </div>
             </div>
         </div>
 
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,8 +1,101 @@
+import { Link } from "react-router-dom";
 
 const Register = () => {
+    const handleRegister = event => {
+        event.preventDefault()
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        const photoUrl = form.photoUrl.value;
+        console.log(name, email, password, photoUrl)
+    }
     return (
-        <div>
-            
+        <div className="flex justify-center items-center h-screen">
+            <div className="max-w-md w-full bg-white p-6 shadow-lg rounded-lg">
+                <h2 className="text-2xl font-bold mb-6">Register</h2>
+                <form onSubmit={handleRegister}>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
+                            Name
+                        </label>
+                        <input
+                            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            name="name"
+                            type="text"
+                            placeholder="Enter your name"
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                            Email
+                        </label>
+                        <input
+                            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            name="email"
+                            type="email"
+                            placeholder="Enter your email"
+
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                            Password
+                        </label>
+                        <input
+                            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            name="password"
+                            type="password"
+                            placeholder="Enter your password"
+
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="photoURL">
+                            Photo URL
+                        </label>
+                        <input
+                            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="photoURL"
+                            name="photoUrl"
+                            type="text"
+                            placeholder="Enter your photo URL"
+
+                        />
+                    </div>
+                    <input type="submit"
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline btn-block"
+                        value="Register"
+                    />
+                    <div className="mt-4 text-center">
+                        <button
+                            className="bg-red-500 hover:bg-red-700 text-white font-semibold py-2 px-4 rounded focus:outline-none btn-block focus:shadow-outline mt-5"
+                            type="button"
+                        >
+                            Sign in with Google
+                        </button>
+                        {/* <span>Or register with: </span>
+                    <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 mx-2 rounded focus:outline-none focus:shadow-outline">
+                        Google
+                    </button> */}
+                    </div>
+                </form>
+                <div className="mt-4 text-center">
+                    <span>Already have an account? <Link className="text-blue-500" to="/login">Login</Link></span>
+                </div>
+                <div className="mt-4 text-center">
+                    {/* <button
+                        className="bg-red-500 hover:bg-red-700 text-white font-semibold py-2 px-4 rounded focus:outline-none btn-block focus:shadow-outline mt-5"
+                        type="button"
+                    >
+                        Sign in with Google
+                    </button> */}
+                    {/* <span>Or register with: </span>
+                    <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 mx-2 rounded focus:outline-none focus:shadow-outline">
+                        Google
+                    </button> */}
+                </div>
+            </div>
         </div>
     );
 };
